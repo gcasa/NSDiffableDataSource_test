9f8759d627ac018495e28f07754162114dca57d5
Gregory Casamento <greg.casamento@gmail.com>
2026-01-03 22:42:30 -0500

    Initial Commit

---
+++ NSDiffableDataSource_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSDiffableDataSource_test
+//
+//  Created by Gregory Casamento on 1/3/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
